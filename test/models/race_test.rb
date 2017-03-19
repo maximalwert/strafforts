123e61dd696d4f2fd5f855c18603ad5e2e5f5c61
Yi Zeng <yi.zeng.dev@gmail.com>
2017-03-19 18:06:58 +1300

    Add missing controller/model tests

--- test/models/race_test.rb
+++ test/models/race_test.rb
@@ -28,17 +28,17 @@ class RaceTest < ActiveSupport::TestCase
     end
   end
 
-  test 'should get nil when Race.find_all_by_athlete_id_and_race_distance_id finds nothing' do
+  test 'should get nil when Race.find_all_by_athlete_id_and_year finds nothing' do
     # Act.
-    item = Race.find_all_by_athlete_id_and_race_distance_id(123, 100)
+    item = Race.find_all_by_athlete_id_and_year(123, 1999)
 
     # Assert.
     assert_nil(item)
   end
 
-  test 'should get all matching items when Race.find_all_by_athlete_id_and_race_distance_id finds few items' do
+  test 'should get all matching items when Race.find_all_by_athlete_id_and_year finds few items' do
     # Act.
-    items = Race.find_all_by_athlete_id_and_race_distance_id(123, 1)
+    items = Race.find_all_by_athlete_id_and_year(123, 2013)
 
     # Assert.
     assert_not(items.empty?)
@@ -46,5 +46,22 @@ class RaceTest < ActiveSupport::TestCase
       assert_equal(123, item.athlete_id)
       assert_equal(1, item.race_distance_id)
     end
+  end
+
+  test 'should get nil when Race.find_years_and_counts_by_athlete_id finds nothing' do
+    # Act.
+    results = Race.find_years_and_counts_by_athlete_id(234)
+
+    # Assert.
+    assert_equal(true, results.empty?)
+  end
+
+  test 'should get all matching items when Race.find_years_and_counts_by_athlete_id finds few items' do
+    # Act.
+    results = Race.find_years_and_counts_by_athlete_id(123)
+
+    # Assert.
+    assert_equal(2013, results[0][0])
+    assert_equal(2, results[0][1])
   end
 end
