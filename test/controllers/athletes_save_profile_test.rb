7ce25111f2b7d98d91b261de037c293581e8a128
Yi Zeng <yi.zeng.dev@gmail.com>
2017-04-28 21:07:54 +1200

    Remove broken tests that requires cookies setup and add new ones

--- test/controllers/athletes_save_profile_test.rb
+++ test/controllers/athletes_save_profile_test.rb
@@ -8,33 +8,10 @@ class AthletesControllerTest < ActionDispatch::IntegrationTest
     assert(exception.message.include?('Could not find requested athlete'))
   end
 
-  test 'should set is_public correctly when POST save_profile' do
-    # Scenario 1.
-    # Currently false, POST without params[:is_public] should still be false.
-    # POST params[:is_public] should become true.
-    athlete1 = Athlete.find_by_id_or_username(123)
-    assert_not(athlete1.is_public)
-
-    post '/athletes/123/save_profile'
-    athlete1.reload
-    assert_not(athlete1.is_public)
-
-    post '/athletes/123/save_profile', params: { is_public: true }
-    athlete1.reload
-    assert(athlete1.is_public)
-
-    # Scenario 2.
-    # Currently true, POST with params[:is_public] should still be true.
-    # POST without params[:is_public] should become false.
-    athlete2 = Athlete.find_by_id_or_username(456)
-    assert(athlete2.is_public)
-
-    post '/athletes/456/save_profile', params: { is_public: true }
-    athlete2.reload
-    assert(athlete2.is_public)
-
-    post '/athletes/456/save_profile'
-    athlete2.reload
-    assert_not(athlete2.is_public)
+  test 'should be a bad request when POST save_profile without a correct access token' do
+    exception = assert_raises(ActionController::BadRequest) do
+      post '/athletes/123/save_profile'
+    end
+    assert(exception.message.include?('Could not update a user that is not the current user'))
   end
 end
