66cc577535778a75ec4a1baa68c32d54f7f1391f
Yi Zeng <yi.zeng.dev@gmail.com>
2017-06-02 17:06:24 +1200

    Lock Chart.js version to 2.5.0 and fix few charts issues
    Lock Chart.js version to 2.5.0 and fix few charts issues
    - Ensure canvas height is set to 300
    - Line charts Y axes should be integers

--- app/assets/javascripts/athletes/helpers/chartHelpers.ts
+++ app/assets/javascripts/athletes/helpers/chartHelpers.ts
@@ -22,7 +22,7 @@ namespace ChartHelpers {
         if (withLoadingIcon) {
             content = HtmlHelpers.getLoadingIcon();
         } else {
-            content = `<canvas id="${id}"></canvas>`;
+            content = `<canvas id="${id}" height="300"></canvas>`;
         }
 
         const chart = `
@@ -59,9 +59,8 @@ namespace ChartHelpers {
 
         const canvasElement = $('#' + id).get(0) as HTMLCanvasElement;
         const ctx = canvasElement.getContext('2d');
-        ctx.canvas.height = 300;
 
-        const linearOptions: Chart.LinearTickOptions = { beginAtZero: true };
+        const linearOptions: Chart.LinearTickOptions = { beginAtZero: true, stepSize: 1 };
         const chart = new Chart(ctx, {
             type: 'bar',
             data: chartData,
@@ -107,7 +106,6 @@ namespace ChartHelpers {
 
         const canvasElement = $('#' + id).get(0) as HTMLCanvasElement;
         const ctx = canvasElement.getContext('2d');
-        ctx.canvas.height = 300;
 
         const chart = new Chart(ctx, {
             type: 'pie',
@@ -181,7 +179,6 @@ namespace ChartHelpers {
 
             const canvasElement = $('#' + id).get(0) as HTMLCanvasElement;
             const ctx = canvasElement.getContext('2d');
-            ctx.canvas.height = 300;
 
             const myLineChart = new Chart(ctx, {
                 type: 'line',
@@ -406,7 +403,6 @@ namespace ChartHelpers {
 
             const canvasElement = $('#' + id).get(0) as HTMLCanvasElement;
             const ctx = canvasElement.getContext('2d');
-            ctx.canvas.height = 300;
 
             const linearOptions: Chart.LinearTickOptions = { beginAtZero: true };
             const chart = new Chart(ctx, {
