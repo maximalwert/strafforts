ddeda427a60798bfa7252d0927363a26e7a7b77a
Yi Zeng <yi.zeng.dev@gmail.com>
2017-06-25 14:29:24 +1200

    Move Google Analytics tracking code to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,20 @@
 //= require jquery2
 //= require jquery_ujs
 //= require bootstrap
+
+// Google Analytics tracking code.
+(function (i, s, o, g, r, a, m) {
+i['GoogleAnalyticsObject'] = r;
+i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)
+},
+i[r].l = 1 * new Date();
+a = s.createElement(o),
+m = s.getElementsByTagName(o)[0];
+a.async = 1;
+a.src = g;
+m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', document.getElementsByTagName('body')[0].getAttribute('data-ga-tracking-id'), 'auto');
+ga('send', 'pageview');
