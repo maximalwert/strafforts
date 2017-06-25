e4ddc64558858facf7e4950349e98a5bbeaba529
Yi Zeng <yi.zeng.dev@gmail.com>
2017-06-25 13:51:37 +1200

    Update Google Analytics event tracking for home and error pages

--- app/assets/javascripts/common/googleAnalyticsHelpers.ts
+++ app/assets/javascripts/common/googleAnalyticsHelpers.ts
@@ -1,27 +1,24 @@
 /// <reference path="./../typings/google.analytics.d.ts" />
 
 namespace GoogleAnalyticsHelpers {
-    export function sendEvent(category: string, action: string, label?: string | null) {
-        if (label) {
-            ga('send', {
-                hitType: 'event',
-                eventCategory: category,
-                eventAction: action,
-                eventLabel: label.trim(),
-            });
-        } else {
-            ga('send', {
-                hitType: 'event',
-                eventCategory: category,
-                eventAction: action,
-            });
-        }
+    export function sendEvent(category: string, action: string, element: Element, value?: number) {
+        const title = element.getAttribute('title') ? element.getAttribute('title') : '';
+        const label = title.length === 0 ? element.textContent.trim() : title;
+        ga('send', {
+            hitType: 'event',
+            eventCategory: category,
+            eventAction: action.trim(),
+            eventLabel: label ? label : null,
+            eventValue: value ? value : null,
+        });
     }
-    export function sendOutboundLinkClickingEvent(href: string) {
+
+    export function sendOutboundLinkClickingEvent(category: string, link: HTMLAnchorElement) {
+        const label = link.getAttribute('href') ? link.getAttribute('href') : link.textContent;
         ga('send', 'event', {
-            eventCategory: 'Outbound Link',
-            eventAction: 'Click',
-            eventLabel: href.trim(),
+            eventCategory: category,
+            eventAction: 'Click Outbound Link',
+            eventLabel: label,
             transport: 'beacon',
         });
     }
