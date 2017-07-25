e426d8b9a8323757dfc697521d91557c84a02bdf
Yi Zeng <yi.zeng.dev@gmail.com>
2017-07-25 21:44:58 +1200

    Add unit and system tests

--- test/system/app/overview_test.rb
+++ test/system/app/overview_test.rb
@@ -8,4 +8,23 @@ class OverviewTest < AppTestBase
     # assert.
     assert_title("#{APP_NAME} | #{DEMO_ATHLETE_NAME} | #{OVERVIEW_TITLE}")
   end
+
+  test 'FAQ panel should load correctly' do
+    # arrange.
+    visit DEMO_URL
+
+    # act.
+    nav_tab_faq = find(:css, App::Selectors::MainContent.nav_tab_faq)
+    nav_tab_faq.click
+
+    # assert.
+    faq_panel = find(:id, 'pane-faqs')
+    within(faq_panel) do
+      headers = all(:css, 'h3.box-title')
+      assert_equal(FAQ_CATEGORIES.count, headers.count)
+      headers.each do |header|
+        assert(FAQ_CATEGORIES.include?(header.text))
+      end
+    end
+  end
 end
