014dbad568f3c42f2ac3e3495dffbacf3f171c16
Yi Zeng <yi.zeng.dev@gmail.com>
2017-07-31 22:32:46 +1200

    Update unit tests

--- test/decorators/athlete_decorator_test.rb
+++ test/decorators/athlete_decorator_test.rb
@@ -24,7 +24,7 @@ class AthleteDecoratorTest < Draper::TestCase
     assert_equal(STRAVA_URL, decorator.follower_url)
   end
 
-  test 'should get "Strava User" when both first and last names are nil' do
+  test 'should get "Strava User" as fullname when both first and last names are nil' do
     athlete = Athlete.find_by_id_or_username(789)
     decorator = AthleteDecorator.decorate(athlete)
     assert_equal('Strava User', decorator.fullname)
@@ -48,6 +48,46 @@ class AthleteDecoratorTest < Draper::TestCase
     assert_equal('Yi Zeng', decorator.fullname)
   end
 
+  test 'should get "Strava User" as display name when both first and last names are nil' do
+    athlete = Athlete.find_by_id_or_username(789)
+    decorator = AthleteDecorator.decorate(athlete)
+    assert_equal('Strava User', decorator.display_name)
+  end
+
+  test 'should get the correct display_name when first or last name is nil' do
+    athlete = Athlete.find_by_id_or_username(789)
+    athlete.firstname = 'Bruce'
+    decorator = AthleteDecorator.decorate(athlete)
+    assert_equal('Bruce', decorator.display_name)
+
+    athlete.firstname = nil
+    athlete.lastname = 'Banner'
+    decorator = AthleteDecorator.decorate(athlete)
+    assert_equal('Banner', decorator.display_name)
+  end
+
+  test 'should get only the first name when fullname is too long' do
+    athlete = Athlete.find_by_id_or_username(789)
+    athlete.firstname = 'Bruce'
+    athlete.lastname = 'veryveryverylonglastname'
+    decorator = AthleteDecorator.decorate(athlete)
+    assert_equal('Bruce', decorator.display_name)
+  end
+
+  test 'should get only the last name when fullname is too long and first name is blank' do
+    athlete = Athlete.find_by_id_or_username(789)
+    athlete.firstname = ''
+    athlete.lastname = 'veryveryveryveryverylonglastname'
+    decorator = AthleteDecorator.decorate(athlete)
+    assert_equal('veryveryveryveryverylonglastname', decorator.display_name)
+  end
+
+  test 'should get the correct display name' do
+    athlete = Athlete.find_by_id_or_username(9123806)
+    decorator = AthleteDecorator.decorate(athlete)
+    assert_equal('Yi Zeng', decorator.display_name)
+  end
+
   test 'should get empty location when both city and country are nil' do
     athlete = Athlete.find_by_id_or_username(789)
     athlete.city_id = nil
@@ -56,16 +96,38 @@ class AthleteDecoratorTest < Draper::TestCase
     assert_equal('', decorator.location)
   end
 
-  test 'should get country when city is nil' do
+  test 'should get empty location when both city and country names are nil' do
+    athlete = Athlete.find_by_id_or_username(789)
+    athlete.city.name = ''
+    athlete.country.name = nil
+    decorator = AthleteDecorator.decorate(athlete)
+    assert_equal('', decorator.location)
+  end
+
+  test 'should get country name when city is nil' do
     athlete = Athlete.find_by_id_or_username(789)
     athlete.city_id = nil
     decorator = AthleteDecorator.decorate(athlete)
     assert_equal('New Zealand', decorator.location)
   end
 
-  test 'should get city when country is nil' do
+  test 'should get country name when city name is blank' do
+    athlete = Athlete.find_by_id_or_username(789)
+    athlete.city.name = ''
+    decorator = AthleteDecorator.decorate(athlete)
+    assert_equal('New Zealand', decorator.location)
+  end
+
+  test 'should get city name when country is nil' do
     athlete = Athlete.find_by_id_or_username(789)
     athlete.country_id = nil
+    decorator = AthleteDecorator.decorate(athlete)
+    assert_equal('Christchurch', decorator.location)
+  end
+
+  test 'should get city name when country name is blank' do
+    athlete = Athlete.find_by_id_or_username(789)
+    athlete.country.name = nil
     decorator = AthleteDecorator.decorate(athlete)
     assert_equal('Christchurch', decorator.location)
   end
@@ -74,6 +136,35 @@ class AthleteDecoratorTest < Draper::TestCase
     athlete = Athlete.find_by_id_or_username(9123806)
     decorator = AthleteDecorator.decorate(athlete)
     assert_equal('Christchurch, New Zealand', decorator.location)
+  end
+
+  test 'should get city name as display_location if location is too long' do
+    athlete = Athlete.find_by_id_or_username(789)
+    athlete.country.name = 'The United States of America'
+    decorator = AthleteDecorator.decorate(athlete)
+    assert_equal('Christchurch', decorator.display_location)
+  end
+
+  test 'should get country name as display_location if location is too long and city is nil' do
+    athlete = Athlete.find_by_id_or_username(789)
+    athlete.city = nil
+    athlete.country.name = 'The United States of America'
+    decorator = AthleteDecorator.decorate(athlete)
+    assert_equal('The United States of America', decorator.display_location)
+  end
+
+  test 'should get country name as display_location if location is too long and city name is blank' do
+    athlete = Athlete.find_by_id_or_username(789)
+    athlete.city.name = ''
+    athlete.country.name = 'The United States of America'
+    decorator = AthleteDecorator.decorate(athlete)
+    assert_equal('The United States of America', decorator.display_location)
+  end
+
+  test 'should get the correct display_location' do
+    athlete = Athlete.find_by_id_or_username(9123806)
+    decorator = AthleteDecorator.decorate(athlete)
+    assert_equal('Christchurch, New Zealand', decorator.display_location)
   end
 
   test 'should get 0 friend_count when it\'s blank' do
