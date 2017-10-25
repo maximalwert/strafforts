97c9334ade122e76d6c230fd6dc3acb331154b23
Yi Zeng <yi.zeng.dev@gmail.com>
2017-10-25 22:40:17 +1300

    Load Hotjar script at different times on home and athlete pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -76,13 +76,3 @@ ga('require', 'pageVisibilityTracker', {
 });
 
 ga('send', 'pageview');
-
-(function(h,o,t,j,a,r){
-  var id = document.getElementsByTagName('body')[0].getAttribute('data-hotjar-id');
-  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-  h._hjSettings={hjid:id,hjsv:6};
-  a=o.getElementsByTagName('head')[0];
-  r=o.createElement('script');r.async=1;
-  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-  a.appendChild(r);
-})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
