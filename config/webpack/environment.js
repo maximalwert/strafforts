e2b8b32d0fd8b29fc141ff77c489ac9678eea456
Yi Zeng <yi.zeng.dev@gmail.com>
2017-11-14 00:16:41 +1300

    Explicitly include ts fils in tsconfig

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,7 +2,6 @@ const { environment } = require('@rails/webpacker')
 
 environment.loaders.set('typescript', {
     test: /.(ts|tsx)$/,
-    exclude: /node_modules|vendor\/bundle/,
     loader: 'ts-loader'
 });
 
