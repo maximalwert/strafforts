4cae8d9cbc2e895d9080b7321a54d2274759d7f2
Yi Zeng <yi.zeng.dev@gmail.com>
2017-11-19 00:28:12 +1300

    Remove legacy unit tests which have been RSpec'ed

--- test/decorators/best_effort_decorator_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class BestEffortDecoratorTest < Draper::TestCase
-  test 'should get the same best effort after decorating' do
-    item = BestEffort.find(1587424799)
-    decorated_item = BestEffortDecorator.decorate(item)
-    assert_equal(item, decorated_item)
-  end
-end
--- test/decorators/race_decorator_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class RaceDecoratorTest < Draper::TestCase
-  test 'should get the same race after decorating' do
-    item = Race.find(1)
-    decorated_item = RaceDecorator.decorate(item)
-    assert_equal(item, decorated_item)
-  end
-end
