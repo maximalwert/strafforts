9475ebc87c285af090c4a7d92594a178e9f02cd4
Yi Zeng <yi.zeng.dev@gmail.com>
2018-02-11 11:08:45 +1300

    Make sure the correct filter button is highlighted in Best Efforts View

--- app/javascript/athletes/views/bestEffortsByDistance.ts
+++ app/javascript/athletes/views/bestEffortsByDistance.ts
@@ -23,6 +23,7 @@ export default class BestEffortsByDistanceView extends BaseView {
         this.createFilterButtons();
 
         if (this.distance) {
+            $(`.best-efforts-filter-buttons .btn[data-race-distance='${this.distance}']`).addClass('active');
             this.createViewTemplate();
             this.createView();
         } else {
